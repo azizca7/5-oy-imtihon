f03f20195ee8162762b085d95a8dd32e8fa28300
azizca7 <ikromovazizbek256@gmail.com>
2025-01-19 18:06:55 +0500

    end

---
+++ js/home.js
@@ -0,0 +1,188 @@
+fetch("https://trello.vimlc.uz/books")
+  .then((response) => response.json())
+  .then((data) => {
+    const bookList = document.getElementById("bookList");
+    data.forEach((book) => {
+      const card = document.createElement("div");
+      card.classList.add("card1");
+
+      card.innerHTML = `
+        <p>Kitob nomi: <span>${book.title}</span></p>
+        <p>Muallifi: <span>${book.author}</span></p>
+        <p>Yili: <span>${book.year}</span></p>
+        <button id="deleteBtn" class="deleteBtn" data-id="${book.id}">delete</button>
+        <button class="editBtn" data-id="${book.id}" data-title="${book.title}" data-author="${book.author}" data-year="${book.year}">tahrirlash</button>
+      `;
+
+      bookList.appendChild(card);
+    });
+
+    const deleteButtons = document.querySelectorAll(".deleteBtn");
+    deleteButtons.forEach((button) => {
+      button.addEventListener("click", function () {
+        const bookId = this.getAttribute("data-id");
+        deleteBook(bookId, this);
+      });
+    });
+
+    const editButtons = document.querySelectorAll(".editBtn");
+    editButtons.forEach((button) => {
+      button.addEventListener("click", function () {
+        const bookId = this.getAttribute("data-id");
+        const bookTitle = this.getAttribute("data-title");
+        const bookAuthor = this.getAttribute("data-author");
+        const bookYear = this.getAttribute("data-year");
+
+        document.getElementById("bookTitle").value = bookTitle;
+        document.getElementById("bookAuthor").value = bookAuthor;
+        document.getElementById("bookYear").value = bookYear;
+
+        const submitButton = document.querySelector(".btn-submit");
+        submitButton.textContent = "Tahrirlash";
+        submitButton.setAttribute("data-id", bookId);
+        submitButton.setAttribute("data-action", "edit");
+      });
+    });
+  })
+  .catch((error) => console.error(error));
+
+function deleteBook(bookId, buttonElement) {
+  document.getElementById("loading").style.display = "flex";
+  fetch(`https://trello.vimlc.uz/books/${bookId}`, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then(() => {
+      buttonElement.parentElement.remove();
+      document.getElementById("loading").style.display = "none";
+    })
+    .catch((error) => {
+      console.error(error);
+      document.getElementById("loading").style.display = "none";
+    });
+}
+document.addEventListener("DOMContentLoaded", function () {
+  const addBookForm = document.getElementById("addBookForm");
+
+  addBookForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const bookTitle = document.getElementById("bookTitle").value;
+    const bookAuthor = document.getElementById("bookAuthor").value;
+    const bookYear = document.getElementById("bookYear").value;
+
+    const bookData = {
+      title: bookTitle,
+      author: bookAuthor,
+      year: bookYear,
+    };
+
+    const submitButton = document.querySelector(".btn-submit");
+
+    if (submitButton.getAttribute("data-action") === "edit") {
+      const bookId = submitButton.getAttribute("data-id");
+      editBook(bookId, bookData);
+    } else {
+      addBook(bookData);
+    }
+  });
+});
+
+function editBook(bookId, bookData) {
+  document.getElementById("loading").style.display = "flex";
+
+  fetch(`https://trello.vimlc.uz/books/${bookId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(bookData),
+  })
+    .then((response) => response.json())
+    .then(() => {
+      fetchBooks();
+      document.getElementById("loading").style.display = "none";
+      const submitButton = document.querySelector(".btn-submit");
+      submitButton.textContent = "Qo'shish";
+      submitButton.removeAttribute("data-id");
+      submitButton.removeAttribute("data-action");
+      addBookForm.reset();
+    })
+    .catch((error) => {
+      console.error(error);
+      document.getElementById("loading").style.display = "none";
+    });
+}
+
+function addBook(bookData) {
+  document.getElementById("loading").style.display = "flex";
+
+  fetch("https://trello.vimlc.uz/books", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(bookData),
+  })
+    .then((response) => response.json())
+    .then(() => {
+      fetchBooks();
+      document.getElementById("loading").style.display = "none";
+    })
+    .catch((error) => {
+      console.error(error);
+      document.getElementById("loading").style.display = "none";
+    });
+}
+
+function fetchBooks() {
+  fetch("https://trello.vimlc.uz/books")
+    .then((response) => response.json())
+    .then((data) => {
+      const bookList = document.getElementById("bookList");
+      bookList.innerHTML = "";
+
+      data.forEach((book) => {
+        const card = document.createElement("div");
+        card.classList.add("card1");
+
+        card.innerHTML = `
+          <p>Kitob nomi: <span>${book.title}</span></p>
+          <p>Muallifi: <span>${book.author}</span></p>
+          <p>Yili: <span>${book.year}</span></p>
+          <button id="deleteBtn" class="deleteBtn" data-id="${book.id}">delete</button>
+          <button class="editBtn" data-id="${book.id}" data-title="${book.title}" data-author="${book.author}" data-year="${book.year}">tahrirlash</button>
+        `;
+
+        bookList.appendChild(card);
+      });
+
+      const deleteButtons = document.querySelectorAll(".deleteBtn");
+      deleteButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+          const bookId = this.getAttribute("data-id");
+          deleteBook(bookId, this);
+        });
+      });
+
+      const editButtons = document.querySelectorAll("#editBtn");
+      editButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+          const bookId = this.getAttribute("data-id");
+          const bookTitle = this.getAttribute("data-title");
+          const bookAuthor = this.getAttribute("data-author");
+          const bookYear = this.getAttribute("data-year");
+
+          document.getElementById("bookTitle").value = bookTitle;
+          document.getElementById("bookAuthor").value = bookAuthor;
+          document.getElementById("bookYear").value = bookYear;
+
+          const submitButton = document.querySelector(".btn-submit");
+          submitButton.textContent = "Tahrirlash";
+          submitButton.setAttribute("data-id", bookId);
+          submitButton.setAttribute("data-action", "edit");
+        });
+      });
+    })
+    .catch((error) => console.error(error));
+}
---
+++ js/login.js
@@ -0,0 +1,91 @@
+const kirishText = document.querySelector(".title-text .kirish");
+const kirishForm = document.querySelector("form.kirish");
+const royxatdanOtishForm = document.querySelector("form.royxatdan-otish");
+const kirishBtn = document.querySelector("label.kirish");
+const royxatdanOtishBtn = document.querySelector("label.royxatdan-otish");
+const royxatdanOtishLink = document.querySelector("form .signup-link a");
+
+function validate(email, password) {
+  const emailTekshiruv = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (!emailTekshiruv.test(email)) {
+    alert("Noto'g'ri email formati!");
+    kirishForm.reset();
+    royxatdanOtishForm.reset();
+    return false;
+  }
+
+  if (password.length < 6) {
+    alert("Parol kamida 6 ta belgidan iborat bo'lishi kerak!");
+    return false;
+  }
+
+  return true;
+}
+
+royxatdanOtishBtn &&
+  royxatdanOtishBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    kirishForm.style.marginLeft = "-50%";
+    kirishText.style.marginLeft = "-50%";
+    document.querySelector(".title-text .kirish").classList.remove("active");
+    document
+      .querySelector(".title-text .royxatdan-otish")
+      .classList.add("active");
+  });
+
+kirishBtn &&
+  kirishBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    kirishForm.style.marginLeft = "0%";
+    kirishText.style.marginLeft = "0%";
+    document
+      .querySelector(".title-text .royxatdan-otish")
+      .classList.remove("active");
+    document.querySelector(".title-text .kirish").classList.add("active");
+  });
+
+royxatdanOtishLink &&
+  royxatdanOtishLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    royxatdanOtishBtn.click();
+  });
+
+kirishForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = document.querySelector("form.kirish input[type='text']").value;
+  const password = document.querySelector(
+    "form.kirish input[type='password']"
+  ).value;
+
+  const storedEmail = localStorage.getItem("email");
+  const storedPassword = localStorage.getItem("password");
+
+  if (validate(email, password)) {
+    if (email === storedEmail && password === storedPassword) {
+      window.location.href = "http://127.0.0.1:5500/pages/home.html";
+    } else {
+      alert("Noto'g'ri email yoki parol!");
+    }
+  }
+});
+
+royxatdanOtishForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = document.querySelector(
+    "form.royxatdan-otish input[type='text']"
+  ).value;
+  const password = document.querySelector(
+    "form.royxatdan-otish input[type='password']"
+  ).value;
+
+  if (validate(email, password)) {
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+
+    window.location.href = "http://127.0.0.1:5500/pages/login.html";
+  }
+});
